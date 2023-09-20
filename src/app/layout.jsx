e3f8b1b997dd4aea1c85a5debbaac2434a4017df
kianafirozian <kianafirozian96@gmail.com>
2023-09-20 21:38:01 +0330

    create the header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,19 @@
+import Header from "@/components/Header";
 import "./globals.css";
+// here we add the header,navbar,search box , ... which is used in all pages
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* header */}
+        <Header />
+
+        {/* navbar */}
+
+        {/* searchbox */}
+        {children}
+      </body>
     </html>
   );
 }
